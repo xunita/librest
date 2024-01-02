bbdf2d036517f3736666d2c74d0dca28c06ae5d3
xunita <yayasopeguesoro@gmail.com>
2024-01-02 23:37:02 +0100

    add external link in footer

--- app/footer.js
+++ app/footer.js
@@ -5,14 +5,16 @@ export default function Footer() {
     <footer className="md:px-24 px-6 text-sm flex items-center justify-center py-4 space-x-2 border-t">
       <span>Développé par</span>{" "}
       <a
-        href="/"
+        href="https://www.linkedin.com/in/sopoude/"
+        _target="blank"
         className="text-sm font-bold border-b border-transparent hover:border-orange-500"
       >
         Yaya Sopegue Soro
       </a>{" "}
       <span>avec</span>{" "}
       <a
-        href="/"
+        href="https://nextjs.org/"
+        _target="blank"
         className="text-sm font-bold border-b border-transparent hover:border-orange-500"
       >
         React.js(Next.Js)
